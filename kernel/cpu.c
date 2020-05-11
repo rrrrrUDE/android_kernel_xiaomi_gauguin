f7f8c1330905049cfbfaa13f1c6a8660d6288f35
Kristof Petho <kristof.petho@gmail.com>
2020-05-11 10:04:24 +0200

    Silence CPU state log in dmesg
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1364,7 +1364,7 @@ int freeze_secondary_cpus(int primary)
 			continue;
 
 		if (pm_wakeup_pending()) {
-			pr_info("Wakeup pending. Abort CPU freeze\n");
+			pr_debug("Wakeup pending. Abort CPU freeze\n");
 			error = -EBUSY;
 			break;
 		}
