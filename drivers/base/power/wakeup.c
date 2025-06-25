87fda2cdc07b8b01b1176a086e002a78f5fcca19
ShirkNeko <109797057+ShirkNeko@users.noreply.github.com>
2025-06-25 14:59:27 +0800

    Restoration of the C90 standard

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -1173,9 +1173,9 @@ static const struct file_operations wakeup_sources_stats_fops = {
 
 static int __init wakeup_sources_debugfs_init(void)
 {
+	int flag = 1;
 	wakeup_sources_stats_dentry = debugfs_create_file("wakeup_sources",
 			S_IRUGO, NULL, NULL, &wakeup_sources_stats_fops);
-	int flag = 1;
 	debugfs_create_file("trace_marker", 0220, debugfs_create_dir("tracing", NULL), &flag, NULL);
         debugfs_create_dir("events",debugfs_create_dir("tracing", NULL));
 	return 0;
