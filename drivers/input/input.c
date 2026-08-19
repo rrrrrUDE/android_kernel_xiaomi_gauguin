3aae9c64104e1d4e3c657821428d0334470e5f0d
rrrrrUDE! <xkandemo666@gmail.com>
2026-08-19 20:09:24 +0800

    Remove KernelSU Tracepoint Hooks Option
    Signed-off-by: rrrrrUDE! <xkandemo666@gmail.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -28,9 +28,6 @@
 #include <linux/mutex.h>
 #include <linux/rcupdate.h>
 #include "input-compat.h"
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
-#include <../../drivers/kernelsu/ksu_trace.h>
-#endif
 
 MODULE_AUTHOR("Vojtech Pavlik <vojtech@suse.cz>");
 MODULE_DESCRIPTION("Input core");
@@ -381,19 +378,10 @@ static int input_get_disposition(struct input_dev *dev,
 	return disposition;
 }
 
-#ifdef CONFIG_KSU
-extern bool ksu_input_hook __read_mostly;
-extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
-#endif
-
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
 	int disposition = input_get_disposition(dev, type, code, &value);
-#ifdef CONFIG_KSU
-	if (unlikely(ksu_input_hook))
-		ksu_handle_input_handle_event(&type, &code, &value);
-#endif
 
 	if (disposition != INPUT_IGNORE_EVENT && type != EV_SYN)
 		add_input_randomness(type, code, value);
@@ -460,10 +448,6 @@ void input_event(struct input_dev *dev,
 		 unsigned int type, unsigned int code, int value)
 {
 	unsigned long flags;
-
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
-	trace_ksu_trace_input_hook(&type, &code, &value);
-#endif
 
 	if (is_event_supported(type, dev->evbit, EV_MAX)) {
 
