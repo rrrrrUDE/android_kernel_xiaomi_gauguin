8f5efd2a6539ef36436f11c55b4abf3918b49f74
rrrrrUDE! <xkandemo666@gmail.com>
2026-08-19 11:47:23 +0800

    Update sys.c
    Signed-off-by: rrrrrUDE! <xkandemo666@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1284,7 +1284,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-if (!strncmp(current->comm, "bpfloader", 9) ||
+    if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netbpfload", 10) ||
 	    !strncmp(current->comm, "netd", 4) ||
 	    !strncmp(current->comm, "uprobestats", 11)) {
